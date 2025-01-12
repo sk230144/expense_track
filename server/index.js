a715e02f39161d8de56589cf7f73f4cbdaab91f5
Saurabh <risabht043@gmail.com>
2025-01-12 15:16:27 +0530

    done

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 import connectMongoDb from './db/connect.js';
 import expenseRoutes from './routes/expense.routes.js'
 import express from 'express'
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
+
 app.use(express.json());
 
 
